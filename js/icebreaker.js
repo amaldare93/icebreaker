7a92addcf2680c87a4c54c5b735204f9a0b3fe57
amaldare93 <amaldare93@gmail.com>
2014-12-05 20:44:05 -0500

    added markers to map and listed nearby places

--- js/icebreaker.js
+++ js/icebreaker.js
@@ -5,9 +5,6 @@ var config = {
     authUrl: 'https://foursquare.com/',
     apiUrl: 'https://api.foursquare.com/'
   };
-  
- 
-  //<![CDATA[
  
   /* Attempt to retrieve access token from URL. */
   function doAuthRedirect() {
@@ -28,35 +25,47 @@ var config = {
   }
  /////////////////////////////////////////////////////////
 
-  /* HTML 5 geolocation. */
-navigator.geolocation.getCurrentPosition(function(data) {
-    var lat = data['coords']['latitude'];
-    var lng = data['coords']['longitude'];
-    /* Create map. */
-    var map = new L.mapbox.map('map', 'hellayela.k81bj929')
-      .setView(new L.LatLng(lat, lng), 15);
+  /* get picture & name */
+  $.getJSON(config.apiUrl + 'v2/users/self?' + '&oauth_token=' + token + '&v=20140601', {}, function(data) {
+    document.getElementById("name").innerHTML = data['response']['user']['firstName'];
+    var picURL = data['response']['user']['photo']['prefix'] + '80x80' + data['response']['user']['photo']['suffix'];
+    document.getElementById("picture").innerHTML = '<img src="' + picURL + '">';
+  })
 
-    /**
-     * This is a sample map url that you need to change.
-     * Sign up at http://mapbox.com/foursquare for a custom map url.
+  
+  navigator.geolocation.getCurrentPosition( function( position ){ 
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+
+    // get list of venues
+    $.getJSON(config.apiUrl + 'v2/venues/search?ll=' + lat + ',' + lng + '&oauth_token=' + token + '&v=20141205', {}, function(data) {
+      venues = data['response']['venues'];
+
+      // Create map.
+      var map = new L.mapbox.map('map', 'hellayela.k81bj929').setView(new L.LatLng(lat, lng), 17);
+
+      for(var i = 0; i < 11; i++){
+        var num = i+1;
+        document.getElementById(i).innerHTML = '<p> ' + num + '. ' + venues[i]['name'] + '<br>';
+        var latLng = new L.LatLng(
+          venues[i]['location']['lat'],
+          venues[i]['location']['lng']
+        );
+        L.marker(latLng).addTo(map);
+      }
+    })
+
+  })
     
-    var mapboxUrl = 'http://a.tiles.mapbox.com/v3/foursquare.map-b7qq4a62.jsonp';
-???? wax.tilejson(mapboxUrl, function(tilejson) {
-      map.addLayer(new wax.leaf.connector(tilejson)); */
-    });
-
-  /* Query foursquare API for venue recommendations near the current location. */
-    $.getJSON(config.apiUrl + 'v2/venues/explore?ll=' + '40.7685' + ',' + '-73.9647' + '&oauth_token=' + 'FGMM54IS2LJSTW1MRKMR4ISX1X4NQCRWZWJWWNMSQO2MZDM5' + '&v=20140601', {}, function(data) {
-      venues = data['response']['groups'][0]['items'];
-
-      /* Place marker for each venue. */
+      /* Place marker for each venue. 
       for (var i = 0; i < venues.length; i++) {
-        /* Get marker's location */
+        /* Get marker's location 
         var latLng = new L.LatLng(
           venues[i]['venue']['location']['lat'],
           venues[i]['venue']['location']['lng']
         );
-        /* Build icon for each icon */
+
+        /* Build icon for each icon 
         var fsqIcon = venues[i]['venue']['categories'][0]['icon'];
         var leafletIcon = L.Icon.extend({
           iconUrl: fsqIcon['prefix'] + '32' + fsqIcon['suffix'],
@@ -65,15 +74,15 @@ navigator.geolocation.getCurrentPosition(function(data) {
           iconAnchor: new L.Point(16, 41),
           popupAnchor: new L.Point(0, -51)
         });
-     
+    
         var icon = new leafletIcon();
         var marker = new L.Marker(latLng, {icon: icon})
           .bindPopup(venues[i]['venue']['name'], { closeButton: false })
           .on('mouseover', function(e) { this.openPopup(); })
           .on('mouseout', function(e) { this.closePopup(); });
         map.addLayer(marker);
-
-        /*
+ 
+        
 var foursquarePlaces = L.layerGroup().addTo(map);
 // Transform each venue result into a marker on the map.
     for (var i = 0; i < result.response.venues.length; i++) {
@@ -91,12 +100,13 @@ var foursquarePlaces = L.layerGroup().addTo(map);
         .addTo(foursquarePlaces);
     }
 
-        */
+        
 
 
 
       }
     })
+*/
   }
 
 
